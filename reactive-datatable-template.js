5e754778c7c4995f4f7f24bf4ea2cbea1f14f530
Steve Steiner <ssteinerX@gmail.com>
2014-11-06 18:32:13 -0500

    Backwards compatible support for sending a function returning a function as "tableData."

--- reactive-datatable-template.js
+++ reactive-datatable-template.js
@@ -2,6 +2,12 @@ Template.ReactiveDatatable.rendered = function () {
 	var data = this.data;
     var datatable = new ReactiveDatatable(data.options, data.id);
     this.autorun(function(){
-        datatable.update(data.tableData());
+    	if (typeof data.tableData === "function"){
+    		console.log("Deprecation warning: tableData should not be a function, it should be the return value of the function that retrieves your data.");
+	        datatable.update(data.tableData());
+	    }
+	    else{
+	    	datatable.update(data.tableData);
+	    }
     });
 };
