45623366c13fffa097dcc0510d88d0b1f8c3e6e6
Steve Steiner <ssteinerX@gmail.com>
2014-11-14 19:27:32 -0500

    Whitespace.

--- package.js
+++ package.js
@@ -8,10 +8,10 @@ Package.onUse(function(api) {
   api.versionsFrom('METEOR@0.9.0');
   api.use(['templating'], 'client');
   api.addFiles([
-  	'jquery.dataTables.min.js',
-  	'reactive-datatables.js',
-  	'reactive-datatable-template.html',
-  	'reactive-datatable-template.js',
+    'jquery.dataTables.min.js',
+    'reactive-datatables.js',
+    'reactive-datatable-template.html',
+    'reactive-datatable-template.js',
   ], 'client');
 });
 
