319652178565eb55ffb31727c4082af356d58fac
Michal Powaga <michalpowaga13@gmail.com>
2014-11-18 18:12:09 +0000

    Allow options to be defined as function

--- reactive-datatable-template.js
+++ reactive-datatable-template.js
@@ -1,6 +1,7 @@
 Template.ReactiveDatatable.rendered = function () {
 	var data = this.data;
-    var datatable = new ReactiveDatatable(data.options);
+	var options = typeof data.options === 'function' ? data.options() : data.options;
+    var datatable = new ReactiveDatatable(options);
     this.autorun(function(){
         datatable.update(data.tableData());
     });
