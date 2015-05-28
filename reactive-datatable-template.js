3da79b494eaf6cb17321d721b114fd7e9ee1a045
Ross Martin <rmartin311@gmail.com>
2015-05-28 13:59:24 -0500

    fix issue with multiple datatables on the same template and paging issue when datatable update occurs

--- reactive-datatable-template.js
+++ reactive-datatable-template.js
@@ -1,7 +1,24 @@
-Template.ReactiveDatatable.rendered = function () {
-	var data = this.data;
-    var datatable = new ReactiveDatatable(data.options);
-    this.autorun(function(){
-        datatable.update(data.tableData());
+Template.ReactiveDatatable.rendered = function() {
+    var data = this.data;
+    var reactiveDataTable = new ReactiveDatatable(data.options);
+
+    // Help Blaze cleanly remove entire datatable when changing template / route by
+    // wrapping table in existing element (#datatable_wrap) defined in the template.
+    var table = document.createElement('table');
+    table.id = 'datatable';
+    table.className = 'table dataTable';
+    
+    // Render the table element and turn it into a DataTable
+    $(this.find('#datatable_wrapper')).append(table);
+    var dt = $(table).DataTable(data.options);
+    reactiveDataTable.datatable = dt;
+
+    dt.on('page.dt', function(e, settings) {
+        var info = dt.page.info();
+        reactiveDataTable.page = info.page;
+    });
+
+    this.autorun(function() {
+        reactiveDataTable.update(data.tableData());
     });
 };
--- reactive-datatables.js
+++ reactive-datatables.js
@@ -1,5 +1,4 @@
-ReactiveDatatable = function (options) {
-	var tableID = "datatable";
+ReactiveDatatable = function(options) {
 	var self = this;
 
 	this.options = options = _.defaults(options, {
@@ -8,31 +7,19 @@ ReactiveDatatable = function (options) {
 		stateSave: true,
 		stateDuration: -1, // Store data for session only
 		pageLength: 5,
-		lengthMenu: [ 3, 5, 10, 50, 100 ],
+		lengthMenu: [3, 5, 10, 50, 100],
 		columnDefs: [{ // Global default blank value to avoid popup on missing data
-			targets: "_all",
-			defaultContent: "–––"
+			targets: '_all',
+			defaultContent: '–––'
 		}],
-
-		stateLoadParams: function ( settings, data ){
+		stateLoadParams: function(settings, data) {
 			// Make it easy to change to the stored page on .update()
 			self.page = data.start / data.length;
 		}
 	});
-
-	// Help Blaze cleanly remove entire datatable when changing template / route by
-	// wrapping table in existing element (#datatable_wrap) defined in the template.
-	var table = document.createElement('table');
-	table.id = tableID;
-	table.className = "table dataTable";
-	
-	// Render the table element and turn it into a DataTable
-	$("#datatable_wrapper").append(table);
-	this.datatable = $(table).DataTable(options);
-
 };
 
-ReactiveDatatable.prototype.update = function (data){
+ReactiveDatatable.prototype.update = function(data) {
 	if (!data) return;
 	var self = this;
 
