49ada5f162cc6d4bf208d8fa8e6fa44c1a549893
Geordie J <geojay@gmail.com>
2015-07-17 14:08:32 +0200

    Update to 1.1.0: Remove any trace of HTML ids from the package to allow multiple datatables on one page

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
   name: 'ephemer:reactive-datatables',
   summary: "Fast and reactive jQuery DataTables using standard Cursors / DataTables API. Supports Bootstrap 3.",
-  version: "1.0.8",
+  version: "1.1.0",
   git: "https://github.com/ephemer/meteor-reactive-datatables.git"
 });
 
